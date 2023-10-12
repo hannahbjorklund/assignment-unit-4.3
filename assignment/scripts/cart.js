38122608d5ed805fc656c8b5abf026f4890c949c
Hannah Bjorklund <bjorklundhannah@gmail.com>
2023-10-11 18:44:56 -1000

    Wrote basket functions and began writing tests for them

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,9 +2,35 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+let basket = [];
 
+function addItem(item){
+    basket.push(item);
+    return true;
+}
 
+function listItems(){
+    for(let i = 0; i < basket.length; i++){
+        console.log(basket[i]);
+    }
+}
 
+function empty(){
+    basket = [];
+}
+
+//Testing addItem
+console.log(`Basket is ${basket}`);
+console.log("Adding 'apples' to the basket:", addItem('apples'));
+console.log(`Basket is ${basket}`);
+
+//Testing listItems
+console.log("Adding more items to basket");
+addItem('bread');
+addItem('soda');
+addItem('salami');
+console.log("Listing all basket items:");
+listItems();
 
 
 
